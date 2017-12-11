4d24149b813c9a91abb30284e0c8ff56f8f4ff85
Soph27 <sophie2727@web.de>
2017-12-11 10:00:14 +0100

    11.12. frueh

--- homework_28_11_17.R
+++ homework_28_11_17.R
@@ -1,6 +1,7 @@
 ###homework 21_11_17################################################################
 ###Applied Spatial Data Analysis with R, Chapter 3: Visualizing Spatial Data in R###
-
+install.packages("sp")
+install.packages("ggplot2")
 library(sp)
 library(lattice)
 library(ggplot2)
@@ -8,54 +9,67 @@ library(ggplot2)
 ###3.1_The traditional Plot System###
 ##3.1.1_Plotting points, lines, polygons and grids##
 
-data(meuse)
+data(meuse) # load data set "meuse"
+summary(meuse)
+# plotting points:
 coordinates(meuse) <- c("x","y")
 plot(meuse)
 title("points")
 
+# plotting lines:
 cc <- coordinates(meuse)
-m.sl <- SpatialLines(list(Lines(list(Line(cc)),"line1")))
+m.sl <- SpatialLines(list(Lines(list(Line(cc)),"line1"))) # create SpatialLines object from list of Lines objects and data.frames
 plot(m.sl)
 title("lines")
+# Spatial Lines object is built by joining up the points in sequence; plot daws resulting zigzags
 
+# plotting polygons:
 data(meuse.riv)
+meuse.riv
 meuse.lst <- list(Polygons(list(Polygon(meuse.riv)),"meuse.riv"))
 meuse.pol <- SpatialPolygons(meuse.lst)
 plot(meuse.pol,col="grey")
 title("polygons")
 
+# convert grid data for Meue bank study area into a SpatialPixels object:
 data(meuse.grid)
+meuse.grid
 coordinates(meuse.grid) <- c("x","y")
 meuse.grid <- as(meuse.grid,"SpatialPixels")
-image(meuse.grid,col="grey")
+image(meuse.grid,col="grey") # display created object by image() with all cells set to "grey"
 title("grid")
 
 image(meuse.grid,col="lightgrey")
-plot(meuse.pol,col="grey",add=TRUE)
-plot(meuse,add=TRUE)
+plot(meuse.pol,col="grey",add=TRUE) # add meuse.grid and meuse.pol together
+plot(meuse,add=TRUE) # add points, polygons and grid together
+
+image(meuse.grid,col="yellow")
+plot(meuse.pol,col="blue",add=TRUE)
+plot(meuse,col="red",add=TRUE)
 
 ##3.1.2_Axes and Layout Elements##
 
 layout(matrix(c(1,2),1,2))
-plot(meuse.pol,axes=TRUE)
-plot(meuse.pol,axes=FALSE)
-axis(1,at=c(178000+0:2*2000),cex.axis=0.7)
-axis(2,at=c(326000+0:3*4000),cex.axis=0.7)
-box()
+plot(meuse.pol,axes=TRUE) # axes with default values
+plot(meuse.pol,axes=FALSE) # axs without default values
+axis(1,at=c(178000+0:2*2000),cex.axis=0.7) # setting of x-axis; cex.axis adjusts size
+axis(2,at=c(326000+0:3*4000),cex.axis=0.7) # setting of y-axis
+box() # box around map
 
-oldpar=par(no.readonly=TRUE)
+oldpar=par(no.readonly=TRUE) # par() for not reserving space for adding axes and titles later on; TRUE means there are no other arguments
 layout(matrix(c(1,2),1,2))
 
-plot(meuse,axes=TRUE,cex=0.6)
+plot(meuse,axes=TRUE,cex=0.6) # R`s default margins leaving space for axes`
 plot(meuse.pol,add=TRUE)
 title("sample locations")
-par(mar=c(0,0,0,0)+0.1)
-
-plot(meuse,axes=FALSE,cex=0.6)
+par(mar=c(0,0,0,0)+0.1) # mar modifies margins
+# par() to set / query grahical parameters
+plot(meuse,axes=FALSE,cex=0.6) # maximised plotting space and no axes drawn
 plot(meuse.pol,add=TRUE)
 box()
 par(oldpar)
 
+# if no axes are drawn on a map, you can plot a scale bar and a north arrow:
 plot(meuse)
 plot(meuse.pol,add=TRUE)
 plot(meuse)
@@ -65,6 +79,12 @@ text(locator(1),"1 km")
 SpatialPolygonsRescale(layout.north.arrow(),offset=locator(1),scale=400,plot.grid=FALSE)
 
 ##3.1.3_Degrees in Axes Labels and Reference Grid##
+# add a grid to a map for reference reasons:
+# code to define and draw projected latitude/longitude grid lines and grid line labels:
+install.packages("rgeos")
+install.packages("maptools")
+install.packages("maps")
+library(rgeos)
 library(maptools)
 library(maps)
 wrld <- map("world",interior=FALSE,xlim=c(-179,179),ylim=c(-89,89),plot=FALSE)
@@ -72,37 +92,47 @@ wrld_p <- pruneMap(wrld,xlim=c(-179,179))
 llCRS <- CRS("+proj=longlat +ellps=WGS84")
 wrld_sp <- map2SpatialLines(wrld_p,proj4string=llCRS)
 prj_new <- CRS("+proj=moll")
+install.packages("rgdal")
 library(rgdal)
 wrld_proj <- spTransform(wrld_sp,prj_new)
+
+# gridLines to generate object of class SpatialLines:
 wrld_grd <- gridlines(wrld_sp,easts=c(-179,seq(-150,150,50),179.5),norths=seq(-75,75,15),ndiscr=100)
 wrld_grd_proj <- spTransform(wrld_grd,prj_new)
-at_sp <- gridat(wrld_sp,easts=0,norths=seq(-75,75,15,offset=0.3))
+at_sp <- gridat(wrld_sp,easts=0,norths=seq(-75,75,15,offset=0.3)) # gridat() returns an object to draw the labels for these 'gridded curves'
 at_proj <- spTransform(at_sp,prj_new)
-plot(wrld_proj,col="grey60")
-plot(wrld_grd_proj,add=TRUE,lty=3,col="grey70")
-text(coordinates(at_proj),pos=at_proj$pos,offset=at_proj$offset,labels=parse(text=as.character(at_proj$labels)),cex=0.6)
+plot(wrld_proj,col="grey60") # plot world map
+plot(wrld_grd_proj,add=TRUE,lty=3,col="grey70") # add coorinate net to world map
+text(coordinates(at_proj),pos=at_proj$pos,offset=at_proj$offset,labels=parse(text=as.character(at_proj$labels)),cex=0.6) # add labels
 
 ##3.1.4_Plot size, plotting area, map scale, and multiple plots##
-
+# R distinguishes between figure region and plotting region
+# get and set figure size in inches:
 par("pin")
 par(pin=c(4,4))
-dev.off()
-X11(width=10,height=10)
-pdf("file.pdf",width=5,height=7)
 
+# for enlarging the plotting window: close current plotting device and re-open it specifying the size:
+dev.off() # shut down the specified device
+windows(width=10,height=10) # x11 opens new window for graphic, or use: x11(widh=10,height=10)
+pdf("file.pdf",width=5,height=7) # write graphic output to a file
+
+# control the plotted data area by passing xlim and ylim in a plot command and setting par():
+# without par(): margins still will be extended by 4% on each side by default
+# set aspect of plotting region equal to that of data points:
 pin <- par("pin")
 dxy <- apply(bbox(meuse),1,diff)
 ratio <- dxy[1]/dxy[2]
 par(pin=c(ratio*pin[2],pin[2]),xaxs="i",yaxs="i")
-plot(meuse,pch=1)
+plot(meuse,pch=1) # plot points without allowing for 4% extention of the range in all directions
 box()
 
-par(mfrow=c(2,3))
+# create more than one map in a single figure by sub-deviding figure region into number of sub-regions:
+par(mfrow=c(2,3)) # split figure into 2 rows and 3 columns
 #or
-layout(matrix(1:6,2,3,byrow=TRUE))
+layout(matrix(1:6,2,3,byrow=TRUE)) # split figure into 2 rows and 3 columns
 
-##3.1.5_Plotting attributes and map legends##
-
+##3.1.5_Plotting attributes and map legends## p.68
+#??????????????????????????????????????????????????????????????zn.idw???????????????????????????????????????????
 grays=grey.colors(4,0.55,0.95)
 image(zn.idw,col=grays,breaks=log(c(100,200,400,800,1400)))
 plot(meuse.pol,add=TRUE)
@@ -110,6 +140,7 @@ plot(meuse,pch=1,cex=sqrt(meuse$zinc)/20,add=TRUE)
 legVals <- c(100,200,500,1000,2000)
 legend("left",legend=legVals,pch=1,pt.cex=sqrt(legVals)/20,bty="n",title="measured")
 legend("topleft",legend=c("100-200","200-400","400-800","800-1800"),fill=grays,bty="n",title="interpolated")
+#????????????????????????????????????????????????????????????????????????????????????????????????????????????????
 
 ###3.2_Trellis/Lattice Plots with spplot###
 ##3.2.1_a straight trellis example##
